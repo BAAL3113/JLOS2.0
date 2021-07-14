2d7449a109f9c4e746e2a08e9fd506e375e1750f
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:25:37 -0600

    Create frmview_stockin.vb

---
+++ JLOS2.0/JLOS2.0/forms/views/frmview_stockin.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmview_stockin
+
+    Private Sub frmview_stockin_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        sql = "SELECT  NUMEROTRANSACCION,concat(NOMBRE, APELLIDOS) as 'PROVEEDOR',NOMBRE as 'NOMBREITEM', DESCRIPCION, PRECIOCOMPRA, PRECIOVENTA, FECHATRANSACCION, i.CANTIDAD, PRECIOTOTAL FROM  tbpersona p,tbstock_in_out o ,tbitems i WHERE  i.ITEMID=o.ITEMID and p.VENDEDORPROVEEDORID=o.VENDEDORPROVEEDORID and p.TIPO = 'Proveedor'"
+        reloadDtg(sql, dtglist)
+    End Sub
+
+    Private Sub txtsearch_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsearch.TextChanged
+        sql = "SELECT  NUMEROTRANSACCION,concat(NOMBRE, APELLIDOS) as 'PROVEEDOR',NOMBRE as 'NOMBREITEM', DESCRIPCION, PRECIOCOMPRA, PRECIOVENTA, FECHATRANSACCION, i.CANTIDAD, PRECIOTOTAL FROM  tbpersona p,tbstock_in_out o ,tbitems i WHERE  i.ITEMID=o.ITEMID and p.VENDEDORPROVEEDORID=o.VENDEDORPROVEEDORID and p.TIPO = 'Proveedor'" &
+        " AND (NOMBRE like '%" & txtsearch.Text & "%' OR  NUMEROTRANSACCION like '%" & txtsearch.Text & "%')"
+        reloadDtg(sql, dtglist)
+    End Sub
+End Class
