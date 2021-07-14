2fc6de4504c43a855a2af858331312b098939e86
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:23:40 -0600

    Create frm_viewTransaction.vb

---
+++ JLOS2.0/JLOS2.0/forms/views/frm_viewTransaction.vb
@@ -0,0 +1,17 @@
+﻿Public Class frm_viewTransaction
+    Private Sub frm_viewTransaction_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        sql = " SELECT NUMEROTRANSACCION,  FECHATRANSACCION,  NOMBRE, APELLIDOS  FROM tbtransaccion t,tbpersona c WHERE  c.CLIENTEPROVEEDORID=t.CLIENTEPROVEEDORID AND t.TIPO ='StockOut'"
+        reloadDtg(sql, dtglist)
+    End Sub
+
+    Private Sub txtsearch_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsearch.TextChanged
+        sql = " SELECT NUMEROTRANSACCION,  FECHATRANSACCION,  NOMBRE, APELLIDOS  FROM tbtransaccion t,tbpersona c WHERE  c.CLIENTEPROVEEDORID=t.CLIENTEPROVEEDORID AND t.TIPO ='StockOut' " &
+        " WHERE NUMEROTRANSACCION LIKE '%" & txtsearch.Text & "%'"
+        reloadDtg(sql, dtglist)
+    End Sub
+
+    Private Sub dtglist_DoubleClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtglist.DoubleClick
+        frm_transaction.txttransactionid.Text = dtglist.CurrentRow.Cells(0).Value
+        Me.Close()
+    End Sub
+End Class
