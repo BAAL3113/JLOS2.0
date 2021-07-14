95a5e8b1a466570f365a0e0d18f34595aeb7ec87
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:29:11 -0600

    Create LSettings.vb

---
+++ JLOS2.0/JLOS2.0/Logica/LSettings.vb
@@ -0,0 +1,43 @@
+﻿Public Class LSettings
+    Dim id As Integer
+    Dim desc, para As String
+    Public Sub New()
+
+    End Sub
+    Public Property MID
+        Get
+            Return id
+
+        End Get
+        Set(value)
+            id = value
+
+        End Set
+    End Property
+    Public Property Mdescripcion
+        Get
+            Return desc
+
+        End Get
+        Set(value)
+            desc = value
+
+        End Set
+    End Property
+    Public Property MPARA
+        Get
+            Return para
+
+        End Get
+        Set(value)
+            para = value
+
+        End Set
+    End Property
+    Public Sub New(ByVal id As Integer, ByVal desc As String, ByVal PARA As String)
+        MID = id
+        Mdescripcion = desc
+        MPARA = PARA
+
+    End Sub
+End Class
