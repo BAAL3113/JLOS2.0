4686a7e337b319a1c0ef628bdd6acdb3ae9e6536
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:26:16 -0600

    Create conexion.vb

---
+++ JLOS2.0/JLOS2.0/include/conexion.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Module conexion
+    Public Function sqldb() As SqlConnection
+        Return New SqlConnection("Data Source=JLCS\SQLEXPRESS;Initial Catalog=JLOS2;Integrated Security=True")
+        'myConn = New SqlConnection("Initial Catalog=Northwind;" & "Data Source=localhost;Integrated Security=SSPI;")
+    End Function
+    Public con As SqlConnection = sqldb()
+    Public Sub conectar()
+        Try
+            con.Open()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Public Sub desconectar()
+        Try
+            If con.State = ConnectionState.Open Then
+                con.Close()
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
