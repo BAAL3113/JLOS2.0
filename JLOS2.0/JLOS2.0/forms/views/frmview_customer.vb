b9d2b63fecf6c0c1a19746d157bc98b491048301
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:24:10 -0600

    Create frmview_customer.vb

---
+++ JLOS2.0/JLOS2.0/forms/views/frmview_customer.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmview_customer
+
+    Private Sub frmview_customer_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        sql = "SELECT  CLIENTEPROVEEDORID as 'CLIENTEID', NOMBRE, APELLIDOS, DIRECCION, TELEFONO, CELULAR, TIPO FROM tbpersona WHERE  TIPO ='Cliente'"
+        reloadDtg(sql, dtglist)
+    End Sub
+
+    Private Sub txtsearch_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsearch.TextChanged
+        sql = "SELECT  CLIENTEPROVEEDORID as 'CLIENTEID', NOMBRE, APELLIDOS, DIRECCION, TELEFONO, CELULAR, TIPO FROM tbpersona WHERE  TIPO ='Cliente' AND  CLIENTEPROVEEDORID LIKE '%" & txtsearch.Text & "%'"
+        reloadDtg(sql, dtglist)
+    End Sub
+
+    Private Sub dtglist_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles dtglist.DoubleClick
+        Try
+            frm_transaction.txt_cusid.Text = dtglist.CurrentRow.Cells(0).Value
+            Me.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
