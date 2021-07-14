b497ff44676f8bf44e2d6757b43c1883b8a83f35
BAAL3113 <BAAL3113@GMAIL.COM>
2021-07-14 09:28:51 -0600

    Create LItems.vb

---
+++ JLOS2.0/JLOS2.0/Logica/LItems.vb
@@ -0,0 +1,85 @@
+﻿Public Class LItems
+    Dim itemid, nom, desc, tipo, unit As String
+    Dim preciocom, precioven, cant As Integer
+
+    Public Sub New()
+
+    End Sub
+    Public Property MItemID
+        Get
+            Return itemid
+        End Get
+        Set(value)
+            itemid = value
+        End Set
+    End Property
+    Public Property MNombre
+        Get
+            Return nom
+        End Get
+        Set(value)
+            nom = value
+        End Set
+    End Property
+    Public Property MDescripcion
+        Get
+            Return desc
+        End Get
+        Set(value)
+            desc = value
+        End Set
+    End Property
+    Public Property MTipo
+        Get
+            Return tipo
+        End Get
+        Set(value)
+            tipo = value
+        End Set
+    End Property
+    Public Property MUnit
+        Get
+            Return unit
+        End Get
+        Set(value)
+            unit = value
+        End Set
+    End Property
+    Public Property MPrecioCompra
+        Get
+            Return preciocom
+        End Get
+        Set(value)
+            preciocom = value
+        End Set
+    End Property
+    Public Property MPrecioventa
+        Get
+            Return precioven
+
+        End Get
+        Set(value)
+            precioven = value
+        End Set
+    End Property
+    Public Property MCantidad
+        Get
+            Return cant
+        End Get
+        Set(value)
+            cant = value
+
+        End Set
+    End Property
+    Public Sub New(ByVal itemid As String, ByVal nom As String, ByVal desc As String, ByVal tipo As String, ByVal unit As String, ByVal preciocom As Integer, ByVal precioven As Integer, ByVal cant As Integer)
+        MItemID = itemid
+        MNombre = nom
+        MDescripcion = desc
+        MTipo = tipo
+        MUnit = unit
+        MPrecioCompra = preciocom
+        MPrecioventa = precioven
+        MCantidad = cant
+
+    End Sub
+End Class
